04c4df34ef918074ea378d4bd158eaf71960fecf
axvg <axvg@users.noreply.github.com>
2025-05-04 10:08:50 -0500

    add variables for hello.sh

--- practicas/dirigidas/03/hello.sh
+++ practicas/dirigidas/03/hello.sh
@@ -3,3 +3,12 @@
 bash --version
 
 echo "Hello, world!"
+
+set -u
+NOMBRE="Nombre"
+readonly PI=3.14159
+export ENV="produccion"
+
+echo "Usuario: $NOMBRE"
+echo "Pi vale: $PI"
+echo "Entorno: $ENV"
